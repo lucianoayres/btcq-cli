674896cfefa98acd6b4a85efc8c5ed361b5e7467
Luciano Ayres <lucianofarias@gmail.com>
2024-09-02 05:41:22 -0300

    refactor: refactor percent change parameters logic

--- src/main.go
+++ src/main.go
@@ -10,24 +10,24 @@ import (
 )
 
 func main() {
-	// Define command-line flags
+	// Define command-line flags with short versions
 	allDataFlag := flag.Bool("all-data", false, "Get all available Bitcoin data")
 	priceFlag := flag.Bool("price", false, "Get the current Bitcoin price")
 	volumeFlag := flag.Bool("volume", false, "Get the 24-hour Bitcoin trading volume")
 	marketCapFlag := flag.Bool("market-cap", false, "Get the Bitcoin market capitalization")
 	athPriceFlag := flag.Bool("ath-price", false, "Get the all-time high (ATH) price of Bitcoin")
 	percentChangeFlags := map[string]*bool{
-		"15-minute": flag.Bool("percent-change-15m", false, "Get the 15-minute percent change in Bitcoin price"),
-		"30-minute": flag.Bool("percent-change-30m", false, "Get the 30-minute percent change in Bitcoin price"),
-		"1-hour":    flag.Bool("percent-change-1h", false, "Get the 1-hour percent change in Bitcoin price"),
-		"6-hour":    flag.Bool("percent-change-6h", false, "Get the 6-hour percent change in Bitcoin price"),
-		"12-hour":   flag.Bool("percent-change-12h", false, "Get the 12-hour percent change in Bitcoin price"),
-		"24-hour":   flag.Bool("percent-change-24h", false, "Get the 24-hour percent change in Bitcoin price"),
-		"7-day":     flag.Bool("percent-change-7d", false, "Get the 7-day percent change in Bitcoin price"),
-		"30-day":    flag.Bool("percent-change-30d", false, "Get the 30-day percent change in Bitcoin price"),
-		"1-year":    flag.Bool("percent-change-1y", false, "Get the 1-year percent change in Bitcoin price"),
+		"15m": flag.Bool("pc-15m", false, "Get the 15-minute percent change in Bitcoin price"),
+		"30m": flag.Bool("pc-30m", false, "Get the 30-minute percent change in Bitcoin price"),
+		"1h":  flag.Bool("pc-1h", false, "Get the 1-hour percent change in Bitcoin price"),
+		"6h":  flag.Bool("pc-6h", false, "Get the 6-hour percent change in Bitcoin price"),
+		"12h": flag.Bool("pc-12h", false, "Get the 12-hour percent change in Bitcoin price"),
+		"24h": flag.Bool("pc-24h", false, "Get the 24-hour percent change in Bitcoin price"),
+		"7d":  flag.Bool("pc-7d", false, "Get the 7-day percent change in Bitcoin price"),
+		"30d": flag.Bool("pc-30d", false, "Get the 30-day percent change in Bitcoin price"),
+		"1y":  flag.Bool("pc-1y", false, "Get the 1-year percent change in Bitcoin price"),
 	}
-	
+
 	flag.Parse()
 
 	// Start loading animation
@@ -75,15 +75,15 @@ func handleAllData(data *api.BitcoinData) {
 	fmt.Printf("24-hour Trading Volume: %.2f USD\n", data.Volume24h)
 	fmt.Printf("Market Capitalization: %.2f USD\n", data.MarketCap)
 	fmt.Printf("All-Time High Price: %.2f USD\n", data.ATHPrice)
-	fmt.Printf("15-minute Percent Change: %.2f%%\n", data.PercentChange15m)
-	fmt.Printf("30-minute Percent Change: %.2f%%\n", data.PercentChange30m)
-	fmt.Printf("1-hour Percent Change: %.2f%%\n", data.PercentChange1h)
-	fmt.Printf("6-hour Percent Change: %.2f%%\n", data.PercentChange6h)
-	fmt.Printf("12-hour Percent Change: %.2f%%\n", data.PercentChange12h)
-	fmt.Printf("24-hour Percent Change: %.2f%%\n", data.PercentChange24h)
-	fmt.Printf("7-day Percent Change: %.2f%%\n", data.PercentChange7d)
-	fmt.Printf("30-day Percent Change: %.2f%%\n", data.PercentChange30d)
-	fmt.Printf("1-year Percent Change: %.2f%%\n", data.PercentChange1y)
+	fmt.Printf("Percent Change in 15m: %.2f%%\n", data.PercentChange15m)
+	fmt.Printf("Percent Change in 30m: %.2f%%\n", data.PercentChange30m)
+	fmt.Printf("Percent Change in 1h: %.2f%%\n", data.PercentChange1h)
+	fmt.Printf("Percent Change in 6h: %.2f%%\n", data.PercentChange6h)
+	fmt.Printf("Percent Change in 12h: %.2f%%\n", data.PercentChange12h)
+	fmt.Printf("Percent Change in 24h: %.2f%%\n", data.PercentChange24h)
+	fmt.Printf("Percent Change in 7d: %.2f%%\n", data.PercentChange7d)
+	fmt.Printf("Percent Change in 30d: %.2f%%\n", data.PercentChange30d)
+	fmt.Printf("Percent Change in 1y: %.2f%%\n", data.PercentChange1y)
 }
 
 func handleVolume(data *api.BitcoinData) {
@@ -100,25 +100,37 @@ func handleATHPrice(data *api.BitcoinData) {
 
 func handlePercentChange(period string, data *api.BitcoinData) {
 	var percentChange float64
+	var timePeriod string
+
 	switch period {
-	case "15-minute":
+	case "15m":
 		percentChange = data.PercentChange15m
-	case "30-minute":
+		timePeriod = "15m"
+	case "30m":
 		percentChange = data.PercentChange30m
-	case "1-hour":
+		timePeriod = "30m"
+	case "1h":
 		percentChange = data.PercentChange1h
-	case "6-hour":
+		timePeriod = "1h"
+	case "6h":
 		percentChange = data.PercentChange6h
-	case "12-hour":
+		timePeriod = "6h"
+	case "12h":
 		percentChange = data.PercentChange12h
-	case "24-hour":
+		timePeriod = "12h"
+	case "24h":
 		percentChange = data.PercentChange24h
-	case "7-day":
+		timePeriod = "24h"
+	case "7d":
 		percentChange = data.PercentChange7d
-	case "30-day":
+		timePeriod = "7d"
+	case "30d":
 		percentChange = data.PercentChange30d
-	case "1-year":
+		timePeriod = "30d"
+	case "1y":
 		percentChange = data.PercentChange1y
+		timePeriod = "1y"
 	}
-	fmt.Printf("%s Percent Change: %.2f%%\n", period, percentChange)
+
+	fmt.Printf("Percent Change in %s: %.2f%%\n", timePeriod, percentChange)
 }
